e9ae86ea9990d26539cc8e908d929fab64a6880e
Imtiyaz0000 <sgs18im705+@gmail.com>
2024-03-13 13:48:07 +0000

    bvv

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import { sort } from "./sort.ts";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <input id="input"
+    <input id="input"></input>
+    <button>lol</button>
+    
   </div>
+  <output />
 `
 
 async function start() {
-  await sort([1])
+  console.log("hi");
+  const app = document.querySelector("div");
+  const input = document.getElementById("input");
+  const output = document.querySelector("output");
+  output.textContent = input!.value;
 }
+
+const button = document.querySelector("button")
+button?.addEventListener("click", start);
